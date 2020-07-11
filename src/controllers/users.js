a8c329fe4958d640c043ee51c74e4e8fe2e0d44f
ywalakamar <barxwells@gmail.com>
2020-07-11 15:00:41 +0300

    chore(encryption): encrypt user password

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,17 +1,18 @@
 import { create, find } from '../models/user';
 import db from '../utils/db';
+import {encrypt, decrypt} from "../middleware/auth"
 
 const register = async (req, res) => {
   const { email, password } = req.body;
-  await db.query(create(email, password));
+  await db.query(create(email, encrypt(password)));
   res.status(201).send({ message: 'User successfully created!' });
 };
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   const { rows } = await db.query(find(email));
-  const data = rows[0];
-  if (password === data.password) {
+  const user = rows[0];
+  if (decrypt(password, user.password)) {
     res.status(200).send({ message: 'login successful!' });
   }
 };
