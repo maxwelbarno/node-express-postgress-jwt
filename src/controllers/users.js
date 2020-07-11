ef1850c4cedbe3266fbfc9729231aa9efaf8b1d9
ywalakamar <barxwells@gmail.com>
2020-07-11 14:21:18 +0300

    feature(user): user login

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,9 +1,19 @@
-import create from "../models/user";
-import db from "../utils/db";
+import { create, find } from '../models/user';
+import db from '../utils/db';
 
 const register = async (req, res) => {
-  const { email, password } = req.body;await db.query(create(email, password));
-  res.status(201).send({message:"User successfully created!"});
+  const { email, password } = req.body;
+  await db.query(create(email, password));
+  res.status(201).send({ message: 'User successfully created!' });
 };
 
-export default register
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const { rows } = await db.query(find(email));
+  const data = rows[0];
+  if (password === data.password) {
+    res.status(200).send({ message: 'login successful!' });
+  }
+};
+
+export { register, login };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,9 @@
-import {Router} from 'express'
-import register from '../controllers/users'
+import { Router } from 'express';
+import { register, login } from '../controllers/users';
 
-const route=Router()
+const route = Router();
 
-route.post('/auth/register', register)
+route.post('/auth/register', register);
+route.post('/auth/login', login);
 
-export default route
+export default route;
